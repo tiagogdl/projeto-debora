cf012f002372e7c0c24e8f16c4bfbae141f434cc
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-04-03 16:35:26 -0300

    Script funcionando

--- Script.js
+++ Script.js
@@ -1,8 +1,13 @@
+
 function abrir(img) {
     var crescer = document.getElementById('crescer')
     var imagem = document.getElementById('img-grande')
 
     crescer.style.display = 'flex'
     imagem.src = img.src
+}
 
+function fechar(img) {
+    var crescer = document.getElementById('crescer')
+    crescer.style.display = 'none'
 }
